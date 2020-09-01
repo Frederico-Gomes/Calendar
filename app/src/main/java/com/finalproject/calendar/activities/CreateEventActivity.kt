8efe2999fa2a5ddbc1e5e90bf10cc44560a738f8
franciscone14 <francisconeluiz@gmail.com>
2020-08-31 21:58:11 -0300

    Tela de criação do evento desenvolvida

---
+++ app/src/main/java/com/finalproject/calendar/activities/CreateEventActivity.kt
@@ -0,0 +1,12 @@
+package com.finalproject.calendar.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.finalproject.calendar.R
+
+class CreateEventActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_event)
+    }
+}
--- app/src/main/java/com/finalproject/calendar/activities/EventDetailActivity.kt
+++ app/src/main/java/com/finalproject/calendar/activities/EventDetailActivity.kt
@@ -1,7 +1,8 @@
-package com.finalproject.calendar
+package com.finalproject.calendar.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.finalproject.calendar.R
 
 class EventDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
